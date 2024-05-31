5d241d217638caa52a27f964707699cb344cc3dd
Anushka Bachhav <131550798+Anushka040604@users.noreply.github.com>
2024-05-31 05:30:53 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using node js,not browser
+
+let name="Anushka";//string datatype
+let age=21;//number
+let isLog=true;//boolean
+let state;
+//null=>standalone value
+//undefined=>variable state is not defined because value is not assigned
+//symbol=>unique
+//bigint
+//object
+console.log(typeof("Anushka"));
+
+console.log(typeof(null));//object
+console.log(typeof undefined);//undefined
---
+++ basic/variables.js
@@ -0,0 +1,10 @@
+const accountId=145
+let accountEmail='anushka.22320176@viit.ac.in'
+var password="123"
+//accountId=2 //not allowed
+console.log(accountId)
+/*
+prefer not to use var
+because of issue in block scope and function scope
+*/ 
+console.table([accountId,accountEmail,password])
