5e5221756464abdfbf7b1500bd2bb3c9b55f1444
Anushka Bachhav <131550798+Anushka040604@users.noreply.github.com>
2024-06-06 08:34:44 +0000

    Add basic JavaScript files

---
+++ basic/functions.js
@@ -0,0 +1,16 @@
+function greetings(n)
+{
+    console.log(`Hello ${n}`)
+}
+const name="Anushka"
+greetings(name)
+
+//Add two number
+
+function addtwonum(n1,n2){//n1,n2-parameters
+    return n1+n2;
+
+}
+let res=addtwonum(3,4)//arguments
+console.log(res)
+//!-Logical Not operator - inverting true to false vice versa
